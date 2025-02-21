c4490584258615d3640a1dcda7c5fdebd0d81ab5
Lucas Soares <147398076+LucasAlface@users.noreply.github.com>
2025-02-21 17:36:16 +0000

    Add files via upload

---
+++ ProgTeste/ProgTeste/LeituraFicheiro.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeituraFicheiro
+{
+
+    public class Simulator
+    {
+        public string? ID { get; set; }
+        public string? GAME { get; set; }
+        public string? PARTNER { get; set; }
+        public string? OBJECTNAME { get; set; }
+        public Boolean COMMUNITY { get; set; }
+        public List<RNS> RNSLIST { get; set; }
+        public List<CoreDataRefs> COREDATAREFSLIST { get; set; }
+
+        public Simulator()
+        {
+
+        }
+
+        public Simulator(string id, string game, string partner, string object_name,
+                         Boolean community, List<RNS> rns, List<CoreDataRefs> coreDatarefs)
+        {
+            ID = id;
+            GAME = game;
+            PARTNER = partner;
+            OBJECTNAME = object_name;
+            COMMUNITY = community;
+            RNSLIST = rns;
+            COREDATAREFSLIST = coreDatarefs;
+
+        }
+
+        public void WriteFile(string fileName, string fileText)
+        {
+            /// <summary>
+            /// Writes text on a file
+            /// "fileName" - Location of the file to put text on
+            /// "fileText" - Text to put on the file
+            /// </summary>
+            File.WriteAllText(fileName, fileText);
+        }
+
+        public void ReadFile(string fileName)
+        {
+            /// <summary>
+            /// Reads a given file and writes it on the console
+            /// "fileName" - Location of the file to be read
+            /// </summary>
+            string read = File.ReadAllText(fileName);
+            Console.WriteLine(read);
+        }
+
+
+    }
+
+    public class RNS
+    {
+        public string? ID { get; set; }
+        public string? NAME { get; set; }
+
+        public RNS()
+        {
+
+        }
+
+        public RNS(string? id, string? name)
+        {
+            ID = id;
+            NAME = name;
+        }
+    }
+
+    public class CoreDataRefs
+    {
+        public string? ID { get; set; }
+        public string? NAME { get; set; }
+
+        public CoreDataRefs()
+        {
+
+        }
+
+        public CoreDataRefs(string? id, string? name)
+        {
+            ID = id;
+            NAME = name;
+        }
+    }
+
+}
---
+++ ProgTeste/ProgTeste/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections;
+using LeituraFicheiro;
+
+
+public class Program
+{
+    // Definitions
+    public static List<Simulator> listSimulator = new List<Simulator>();
+    public static List<RNS> listRns = new List<RNS>();
+    public static List<CoreDataRefs> listCoreDataRefs = new List<CoreDataRefs>();
+    public static string file = @"C:\Users\Utilizador\Desktop\ficheiro6.json";
+
+    public static void Main()
+    {
+        /// <summary>
+        /// Writes on a file and reads it
+        /// </summary>
+        WriteFile();
+        ReadFile();
+    }
+
+
+    public static void WriteFile()
+    {
+        /// <summary>
+        /// Calls the "Serialize()" and calls the "WriteFile()" of the Simulator class
+        /// </summary>
+        Simulator sim = new Simulator();
+        string json = Serialize();
+
+        sim.WriteFile(file, json);
+    }
+
+    public static void ReadFile()
+    {
+        /// <summary>
+        /// Calls the "ReadFile" from the Simulator class
+        /// </summary>
+        Simulator sim = new Simulator();
+
+        sim.ReadFile(file);
+    }
+
+    public static string Serialize()
+    {
+        /// <summary>
+        /// Calls the "PopulateLists()" and serializes the created list
+        /// </summary>
+        PopulateLists();
+        return JsonConvert.SerializeObject(listSimulator, Formatting.Indented);
+    }
+
+    public static void PopulateLists()
+    {
+        /// <summary>
+        /// Populates the RNS, CoreDataRefs and Simulator's lists by calling their respective adding object method
+        /// </summary>
+        AddRNS("adf", "XPLANE DATAREF");
+        AddRNS("xpdr", "XPLANE DATAREF");
+        AddRNS("com1_active", "XPLANE DATAREF");
+        AddRNS("com1_stdby", "XPLANE DATAREF");
+        AddRNS("nav1_active", "XPLANE DATAREF");
+        AddRNS("nav1_stdby", "XPLANE DATAREF");
+
+        AddCoreDataRefs("b742/lights/beacon/batatas", "Beacon Light");
+        AddCoreDataRefs("b742/lights/nav/batatas", "Nav Light");
+
+        AddSimulator("66ccc36b08515dfee1b48849", "xplane-12", "felix", "Boeing 742", false, listRns, listCoreDataRefs);
+    }
+
+    public static void AddRNS(string id, string name)
+    {
+        /// <summary>
+        /// Adds an RNS to the list with the given parameters
+        /// "id" - id of the RNS
+        /// "name" - name of the RNS
+        /// </summary>
+        RNS r = new  RNS(id, name);
+        listRns.Add(r);
+    }
+
+    public static void AddCoreDataRefs(string id, string name)
+    {
+        /// <summary>
+        /// Adds a CoreDataRefs to the list with the given parameters
+        /// "id" - id of the CoreDataRefs
+        /// "name" - name of the CoreDataRefs
+        /// </summary>
+        CoreDataRefs c = new  CoreDataRefs(id, name);
+        listCoreDataRefs.Add(c);
+    }
+
+    public static void AddSimulator(string id, string game, string partner, string objectName,
+          Boolean community, List<RNS> rns, List<CoreDataRefs> coreDatarefs)
+    {
+        /// <summary>
+        /// Adds a Simulator to the list with the given parameters
+        /// "id" - id of the Simulator
+        /// "game" - game of the Simulator
+        /// "partner" - partner of the Simulator
+        /// "objectName" - Nome of the object of the Simulator
+        /// "community" - Has a community or not
+        /// "rns" - List of the RNS's of the Simulator
+        /// "coreDatarefs" - List of the Core Data Refs of the Simulator
+        /// </summary>
+        Simulator s = new Simulator(id, game, partner, objectName, community, rns, coreDatarefs);
+        listSimulator.Add(s);
+    }
+
+}
+
+
